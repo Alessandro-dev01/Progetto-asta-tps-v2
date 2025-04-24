5866c7036e562119f783d3b182f1f9cbb132a9ba
congiustiA <s.alessandro.congiusti@polofermigiorgi.it>
2025-04-24 13:01:14 +0200

    socket

--- asta online tps/src/Comunicazione/Sever.java
+++ asta online tps/src/Comunicazione/Sever.java
@@ -1,4 +1,34 @@
 package Comunicazione;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Sever {
+
+    private ServerSocket server;
+    private Connection con;
+    private String DB_URL = "jdbc:mysql://127.0.0.1/asteOnline";
+    private String password = "";
+    private String user = "root";
+
+    public Sever(Connection con) {
+        //connessione per scambio dati prodotti con il client
+        try {
+            this.server = new ServerSocket(5000);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //dati connessioe database
+        try {
+            this.con = DriverManager.getConnection(this.DB_URL,this.user,this.password);
+        } catch (SQLException e) {
+            //fare inoltro messaggio d'errore tramite xml al client
+            System.out.println("errore con la connessione al server");
+        }
+
+    }
 }
--- asta online tps/src/Comunicazione/client.java
+++ asta online tps/src/Comunicazione/client.java
@@ -1,15 +1,29 @@
 package Comunicazione;
+import StrutturaOggetti.Prodotto;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
 
 public class client {
 
     private int id;
+    private String username;
     private Socket connSer;
-    private MulticastSocket asta;
+    private LinkedList<Prodotto> prodotti;
 
+    public client(String username) {
+        this.username = username;
+        try {
+            this.connSer = new Socket(InetAddress.getByName("127.0.0.1"),5000);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.prodotti = new LinkedList<Prodotto>();
+    }
 
-
-
+    
 }
